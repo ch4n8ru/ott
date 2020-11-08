f5ac29ba172b3778cfb0d18ac69f18fff6031c4b
chandra <prakashchandra.cp@outlook.com>
2020-11-08 22:36:38 +0530

    added permission errors

--- src/app/helpers/auth-guard.service.ts
+++ src/app/helpers/auth-guard.service.ts
@@ -1,32 +1,49 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Store } from '@ngrx/store';
 import { UserRights } from '../data/models/auth';
 import { AppState } from '../data/state';
 import { SetRedirectURL } from '../data/state/auth/auth.action';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
-
-  constructor(private store: Store, public router: Router) { }
+  constructor(
+    private store: Store,
+    public router: Router,
+    private toastService: ToastrService
+  ) {}
 
   /**
    * Protects accessing the user route without authentication
-   * @param route 
-   * @param routestate 
+   * @param route
+   * @param routestate
    */
-  canActivate(route: ActivatedRouteSnapshot, routestate: RouterStateSnapshot): boolean {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    routestate: RouterStateSnapshot
+  ): boolean {
     let userRight: UserRights;
-    this.store.select((appState: AppState) => appState.Auth.user).subscribe(user => userRight = user ? user.rights : null)
-    if (userRight == UserRights.VIEW) {
-      return true
+    this.store
+      .select((appState: AppState) => appState.Auth.user)
+      .subscribe((user) => (userRight = user ? user.rights : null));
+    if (userRight) {
+      if (userRight == UserRights.VIEW) {
+        return true;
+      }
+      else{
+        this.toastService.error( "Check if this account has the right permissions" , "Permission Error" )
+      }
     }
-    
     this.store.dispatch(new SetRedirectURL(routestate.url));
     this.router.navigate(['login']);
-    return false
+    return false;
   }
 }
-
--- src/app/helpers/permguard.service.ts
+++ src/app/helpers/permguard.service.ts
@@ -1,30 +1,51 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Store } from '@ngrx/store';
 import { UserRights } from '../data/models/auth';
 import { AppState } from '../data/state';
 import { SetRedirectURL } from '../data/state/auth/auth.action';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PermguardService implements CanActivate {
-
-  constructor(private store: Store ,public router: Router) { }
-/**
- * Protects the admin route by checking the access rights
- * @param route 
- * @param routestate 
- */
-  canActivate(route: ActivatedRouteSnapshot, routestate: RouterStateSnapshot):boolean {
+  constructor(
+    private store: Store,
+    public router: Router,
+    private toastService: ToastrService
+  ) {}
+  /**
+   * Protects the admin route by checking the access rights
+   * @param route
+   * @param routestate
+   */
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    routestate: RouterStateSnapshot
+  ): boolean {
     let userRight: UserRights;
-    this.store.select( (appState:AppState) => appState.Auth.user).subscribe(user => userRight = user.rights )
-    if(userRight == UserRights.FULL)
-   { 
-     return true
-        }
+    this.store
+      .select((appState: AppState) => appState.Auth.user)
+      .subscribe((user) => (userRight = user.rights));
+    if (userRight) {
+      if (userRight == UserRights.FULL) {
+        return true;
+      } else {
+        this.toastService.error(
+          'Check if this account has the right permissions',
+          'Permission Error'
+        );
+      }
+    }
     this.store.dispatch(new SetRedirectURL(routestate.url));
     this.router.navigate(['login']);
-    return false
+    return false;
   }
 }
